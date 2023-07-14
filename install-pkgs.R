454cc459723f9fe88a681bbe615f69b30c4da9a2
rrchai <73901500+rrchai@users.noreply.github.com>
2023-07-14 18:57:57 -0400

    deploy dummy app to shinyapps.io using gh wf

---
+++ install-pkgs.R
@@ -0,0 +1,10 @@
+cran <- c(
+    "shiny",
+    "yaml",
+    "reticulate",
+    "httr"
+)
+
+options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/latest", getOption("repos")))
+
+install.packages(cran)
