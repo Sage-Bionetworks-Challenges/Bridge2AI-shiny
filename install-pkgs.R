bb1828394181ef578edc0e20fc6d1c7786b6c524
rrchai <73901500+rrchai@users.noreply.github.com>
2023-07-24 11:01:30 -0400

    downgrade rsconnect version

--- install-pkgs.R
+++ install-pkgs.R
@@ -4,7 +4,7 @@ cran <- c(
     "shinydashboardPlus",
     "reticulate",
     "httr",
-    "rsconnect",
+    # "rsconnect",
     "jsonlite",
     # "bslib",
     # "waiter",
@@ -17,5 +17,8 @@ cran <- c(
 )
 
 options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/latest", getOption("repos")))
-
 install.packages(cran)
+
+# downgrade rsconnect to avoid the bug while deployment
+install.packages("remotes")
+remotes::install_version("rsconnect", "0.8.29")
