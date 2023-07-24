d9c5a8d3f864702849e4ee6ca79f43b441777a1d
rrchai <73901500+rrchai@users.noreply.github.com>
2023-07-24 10:50:52 -0400

    change back to use .env

--- global.R
+++ global.R
@@ -12,63 +12,62 @@ suppressPackageStartupMessages({
   library(shinydashboardPlus)
   library(ggplot2)
   # library(stringr)
-  library(viridis)  # Load the 'viridis' package for the 'inferno' color palette
+  library(viridis) # Load the 'viridis' package for the 'inferno' color palette
   library(ggsci)
 })
 
 ## Set Up OAuth
-readRenviron(".Renviron")
 stopifnot(
-    !is.null(client_id <- Sys.getenv("CLIENT_ID")) && nchar(client_id) > 0,
-    !is.null(client_secret <- Sys.getenv("CLIENT_SECRET")) && nchar(client_secret) > 0,
-    !is.null(app_url <- Sys.getenv("APP_URL")) && nchar(app_url) > 0
+  !is.null(client_id <- Sys.getenv("CLIENT_ID")) && nchar(client_id) > 0,
+  !is.null(client_secret <- Sys.getenv("CLIENT_SECRET")) && nchar(client_secret) > 0,
+  !is.null(app_url <- Sys.getenv("APP_URL")) && nchar(app_url) > 0
 )
 
 # update port if running app locally
 if (interactive()) {
-    port <- httr::parse_url(app_url)$port
-    if (is.null(port)) stop("running locally requires a TCP port that the application should listen on")
-    options(shiny.port = as.numeric(port))
+  port <- httr::parse_url(app_url)$port
+  if (is.null(port)) stop("running locally requires a TCP port that the application should listen on")
+  options(shiny.port = as.numeric(port))
 }
 
 has_auth_code <- function(params) {
-    # params is a list object containing the parsed URL parameters. Return TRUE if
-    # based on these parameters, it looks like auth code is present that we can
-    # use to get an access token. If not, it means we need to go through the OAuth
-    # flow.
-    return(!is.null(params$code))
+  # params is a list object containing the parsed URL parameters. Return TRUE if
+  # based on these parameters, it looks like auth code is present that we can
+  # use to get an access token. If not, it means we need to go through the OAuth
+  # flow.
+  return(!is.null(params$code))
 }
 
 app <- oauth_app("shinysynapse",
-    key = client_id,
-    secret = client_secret,
-    redirect_uri = app_url
+  key = client_id,
+  secret = client_secret,
+  redirect_uri = app_url
 )
 
 # These are the user info details ('claims') requested from Synapse:
 claims <- list(
-    family_name = NULL,
-    given_name = NULL,
-    email = NULL,
-    email_verified = NULL,
-    userid = NULL,
-    orcid = NULL,
-    is_certified = NULL,
-    is_validated = NULL,
-    validated_given_name = NULL,
-    validated_family_name = NULL,
-    validated_location = NULL,
-    validated_email = NULL,
-    validated_company = NULL,
-    validated_at = NULL,
-    validated_orcid = NULL,
-    company = NULL
+  family_name = NULL,
+  given_name = NULL,
+  email = NULL,
+  email_verified = NULL,
+  userid = NULL,
+  orcid = NULL,
+  is_certified = NULL,
+  is_validated = NULL,
+  validated_given_name = NULL,
+  validated_family_name = NULL,
+  validated_location = NULL,
+  validated_email = NULL,
+  validated_company = NULL,
+  validated_at = NULL,
+  validated_orcid = NULL,
+  company = NULL
 )
 
 claimsParam <- toJSON(list(id_token = claims, userinfo = claims))
 api <- oauth_endpoint(
-    authorize = paste0("https://signin.synapse.org?claims=", claimsParam),
-    access = "https://repo-prod.prod.sagebase.org/auth/v1/oauth2/token"
+  authorize = paste0("https://signin.synapse.org?claims=", claimsParam),
+  access = "https://repo-prod.prod.sagebase.org/auth/v1/oauth2/token"
 )
 
 # The 'openid' scope is required by the protocol for retrieving user information.
@@ -91,4 +90,3 @@ reticulate::use_virtualenv(file.path(getwd(), ".venv"), required = TRUE)
 
 # import synapse client
 syn <- import("synapseclient")$Synapse()
-
