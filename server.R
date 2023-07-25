44d435277146d23a8f68c2e0e476034df6341710
rrchai <73901500+rrchai@users.noreply.github.com>
2023-07-25 00:38:13 -0400

    enhance the UI

--- server.R
+++ server.R
@@ -5,27 +5,27 @@
 # using the session token.  https://www.synapse.org
 
 shinyServer(function(input, output, session) {
-    params <- parseQueryString(isolate(session$clientData$url_search))
-    if (!has_auth_code(params)) {
-        return()
-    }
-    redirect_url <- paste0(
-        api$access, "?", "redirect_uri=", app_url, "&grant_type=",
-        "authorization_code", "&code=", params$code
-    )
-    # get the access_token and userinfo token
-    req <- POST(redirect_url, encode = "form", body = "", authenticate(app$key, app$secret,
-        type = "basic"
-    ), config = list())
-    # Stop the code if anything other than 2XX status code is returned
-    stop_for_status(req, task = "get an access token")
-    token_response <- content(req, type = NULL)
-    access_token <- token_response$access_token
-
-    session$userData$access_token <- access_token
-
-    # Login to synapse
-    syn$login(authToken = access_token, rememberMe = FALSE)
+    # params <- parseQueryString(isolate(session$clientData$url_search))
+    # if (!has_auth_code(params)) {
+    #     return()
+    # }
+    # redirect_url <- paste0(
+    #     api$access, "?", "redirect_uri=", app_url, "&grant_type=",
+    #     "authorization_code", "&code=", params$code
+    # )
+    # # get the access_token and userinfo token
+    # req <- POST(redirect_url, encode = "form", body = "", authenticate(app$key, app$secret,
+    #     type = "basic"
+    # ), config = list())
+    # # Stop the code if anything other than 2XX status code is returned
+    # stop_for_status(req, task = "get an access token")
+    # token_response <- content(req, type = NULL)
+    # access_token <- token_response$access_token
+    # 
+    # session$userData$access_token <- access_token
+    # 
+    # # Login to synapse
+    # syn$login(authToken = access_token, rememberMe = FALSE)
     
     output$user <- renderUser({
       dashboardUser(
@@ -85,70 +85,75 @@ shinyServer(function(input, output, session) {
       addClass("step-3", "complete-step")
       updateTabsetPanel(session, "tabs", selected = "tab4")
     })
-    observeEvent(input$`next-btn-4`, {
-      
+    observeEvent(input$`submit-btn`, {
+      shiny::showNotification(
+        "Submission is not supported yet ~",
+        type = "message",
+        duration = 5
+      )
     })
     
-    ## tab2 plots effect
-    onevent("hover", "option-a-box", {
-      toggleClass("option-a-box", "option-a-hover")
-    })
-    
-    onevent("hover", "option-b-box", {
-      toggleClass("option-b-box", "option-b-hover")
-    })
     
     onevent("click", "option-a-box", {
-      toggleClass("option-a-box", "option-a-selected")
-      removeClass("option-b-box", "option-b-selected")
-      
-      runjs("var is_a = $('#option-a-box').hasClass('option-a-selected'); 
+      toggleClass("option-a-box", "selected")
+      removeClass("option-b-box", "selected")
+      runjs("setTimeout(function() {
+                window.scrollTo(0,document.body.scrollHeight);
+            }, 200);")
+      runjs("var is_a = $('#option-a-box').hasClass('selected'); 
          if (is_a) Shiny.onInputChange('tab2_answer', 'A')
          else Shiny.onInputChange('tab2_answer', null);")
     })
     
     onevent("click", "option-b-box", {
-      toggleClass("option-b-box", "option-b-selected")
-      removeClass("option-a-box", "option-a-selected")
-      runjs("var is_b = $('#option-b-box').hasClass('option-b-selected'); 
+      toggleClass("option-b-box", "selected")
+      removeClass("option-a-box", "selected")
+      runjs("setTimeout(function() {
+                window.scrollTo(0,document.body.scrollHeight);
+            }, 200);")
+      runjs("var is_b = $('#option-b-box').hasClass('selected'); 
          if (is_b) Shiny.onInputChange('tab2_answer', 'B')
          else Shiny.onInputChange('tab2_answer', null);")
     })
     
+    # output$`selected-option-text` <- renderUI({
+    #   req(input$tab2_answer)
+    #   option_color <- ifelse(input$tab2_answer == "A", "option-a-color",  "option-b-color")
+    #   
+    #   # Scroll to the bottom to see the text (selection)
+    #   runjs("setTimeout(function() {
+    #             window.scrollTo(0,document.body.scrollHeight);
+    #         }, 200);")
+    #   
+    #   p("🎉 Woo-hoo! You've chosen option ",
+    #     span(
+    #       class = option_color,
+    #       input$tab2_answer
+    #     )
+    #   )
+    # })
     
-    output$`selected-option-text` <- renderUI({
-      req(input$tab2_answer)
-      option_color <- ifelse(input$tab2_answer == "A", "option-a-color",  "option-b-color")
-      
-      # Scroll to the bottom to see the text (selection)
-      runjs("setTimeout(function() {
-                window.scrollTo(0,document.body.scrollHeight);
-            }, 200);")
-      
-      p("🎉 Woo-hoo! You've chosen option ",
-        span(
-          class = option_color,
-          input$tab2_answer
-        )
-      )
-    })
-    
+    plot_theme <-  
+      theme_minimal(base_size = 14) +
+      theme(
+        legend.position = c(0.95, 0.4),
+        plot.background = element_rect(fill = "#f5f5f5", color = NA)
+    )
+
     data(diamonds)
     output[["option-a-plot"]] <- renderPlot({
       ggplot(diamonds, aes(x = carat, y = price, color = color)) +
         geom_point() +
         scale_color_manual(values = inferno(nlevels(diamonds$color))) +
         labs(x = "Carat", y = "Price") +
-        theme_minimal(base_size = 16) +
-        theme(legend.position = "top")
+        plot_theme
     })
     
     output[["option-b-plot"]] <- renderPlot({
       ggplot(diamonds, aes(x = carat, y = price, color = color)) +
         geom_point() +
         labs(x = "Carat", y = "Price") +
-        theme_minimal(base_size = 16) +
-        theme(legend.position = "top")
+        plot_theme
     })
     
     
@@ -157,24 +162,23 @@ shinyServer(function(input, output, session) {
         geom_point() +
         scale_color_manual(values = as.character(ggsci:::ggsci_db[[input$tab3_answer]][[1]])) +
         labs(x = "Carat", y = "Price") +
-        theme_minimal(base_size = 16) +
-        theme(legend.position = "top")
+        plot_theme
     })
     
     
     output$`q1-answer` <- renderUI({
       if (is.null(input$tab2_answer)) {
-        h4("Please go to the 'A/B Test' to select your answer")
+        h4("🚫 Please choose your preferred option in the 'A/B Test'", class = "error")
       } else {
-        h4(sprintf(
-          "Your have selected %s", dQuote(input$tab2_answer)
-        ))
+        strong(h4(sprintf(
+          "🎉  Woo-hoo! You've chosen option %s", dQuote(input$tab2_answer)
+        )))
       }
     })
     
     output$`q2-answer` <- renderUI({
-      h4(sprintf(
-        "Your have selected %s", dQuote(input$tab3_answer)
-      ))
+      strong(h4(sprintf(
+        "🎉  Woo-hoo! You've chosen option %s", dQuote(input$tab3_answer)
+      )))
     })
 })
--- ui.R
+++ ui.R
@@ -73,7 +73,7 @@ ui <- dashboardPage(
            Morbi feugiat tellus eget turpis rhoncus, sit amet eleifend arcu convallis. Mauris porttitor vel ante non commodo. Sed vehicula fermentum ex non tempus. Mauris vel egestas arcu. Etiam nec sapien pretium, iaculis ipsum vitae, dignissim dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae nulla dictum, accumsan ipsum non, porttitor massa. Maecenas consectetur ultricies metus at pulvinar. Sed convallis augue eget ullamcorper aliquet. Pellentesque erat lectus, laoreet vel tortor nec, porttitor euismod risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras orci nibh, efficitur vel est sit amet, eleifend semper leo. Fusce consequat elit elementum ipsum pulvinar, non consequat est posuere. Ut pharetra dui in sodales euismod. Aliquam placerat maximus dolor at aliquet.\n\n
            Maecenas luctus neque id nulla blandit blandit. Cras consequat condimentum dictum. Aenean eu mattis justo. Proin gravida urna vel maximus aliquet. Suspendisse tincidunt dictum velit at pulvinar. Ut tincidunt massa tortor, vitae viverra ipsum pellentesque ac. Aenean laoreet varius turpis, eget consectetur elit rhoncus eu. Sed blandit volutpat ipsum, et congue mauris luctus accumsan. Ut ultricies velit vel pretium scelerisque. Praesent placerat orci in justo suscipit varius. Nulla facilisi. Praesent et arcu ut dui cursus rutrum."
           ),
-          br(), br(), br(),
+          br(), br(),
           fluidRow(
             column(
               width = 12,
@@ -84,46 +84,39 @@ ui <- dashboardPage(
         ),
         tabItem(
           tabName = "tab2",
-          h2("🎆 Which plot POPS the most? A or B?", align = "center"),
+          h2("🎆 Which plot is more visually appealing, A or B?", align = "center"),
           br(),
-          fluidRow(
-            column(width = 1),
-            column(
-              width = 10,
+          div(
+            class = "ab-test-container",
+            tagList(
               div(
-                class = "ab-test-container",
+                id = "option-a-box",
+                width = 6,
                 tagList(
-                  column(
-                    id = "option-a-box",
-                    width = 6,
-                    tagList(
-                      plotOutput("option-a-plot"),
-                      span("A", id = "option-a-text")
-                    )
-                  ),
-                  div(class = "vs-cycle", "VS"),
-                  column(
-                    id = "option-b-box",
-                    width = 6,
-                    tagList(
-                      plotOutput("option-b-plot"),
-                      span("B", id = "option-b-text")
-                    )
-                  )
-                ),
+                  plotOutput("option-a-plot"),
+                  span("A", class = "option-text")
+                )
+              ),
+              # div(class = "vs-cycle", "VS"),
+              div(
+                id = "option-b-box",
+                width = 6,
+                tagList(
+                  plotOutput("option-b-plot"),
+                  span("B", class = "option-text")
+                )
               )
-            ),
-            column(width = 1)
-          ),
-          br(),
-          fluidRow(
-            column(
-              align = "center",
-              width = 12,
-              uiOutput("selected-option-text")
             )
           ),
-          br(), br(), br(),
+          # br(),
+          # fluidRow(
+          #   column(
+          #     align = "center",
+          #     width = 12,
+          #     uiOutput("selected-option-text")
+          #   )
+          # ),
+          br(), br(),
           fluidRow(
             column(
               width = 12,
@@ -143,8 +136,8 @@ ui <- dashboardPage(
             ),
             column(width = 1)
           ),
-          br(),
-          h2("Color me creative! 🎨 Pick your perfect scatter plot palettes", align = "center"),
+          br(), br(),
+          h2("🎨 Pick the perfect palettes to color scatter plot:", align = "center"),
           br(),
           fluidRow(
             column(width = 1),
@@ -159,7 +152,7 @@ ui <- dashboardPage(
             ),
             column(width = 1)
           ),
-          br(), br(), br(),
+          br(), br(),
           fluidRow(
             column(
               width = 12,
@@ -170,14 +163,14 @@ ui <- dashboardPage(
         ),
         tabItem(
           tabName = "tab4",
-          # h2("Review and submit your answers", align = "center"),
+          h2("Please review and submit your answers", align = "center"),
+          br(),
           fluidRow(
             column(width = 1),
             column(
               width = 10,
               tagList(
-                h3("Which plot POPS the most? A or B?"),
-                br(),
+                h5("Which plot is more visually appealing, A or B?", class = "grey"),
                 uiOutput("q1-answer", class = "answer-text"),
               )
             )
@@ -187,17 +180,16 @@ ui <- dashboardPage(
             column(
               width = 10,
               br(), br(),
-              h3("Pick your perfect scatter plot palettes:"),
-              br(),
+              h5("Pick the perfect palettes to color scatter plot:", class = "grey"),
               uiOutput("q2-answer", class = "answer-text")
             )
           ),
-          br(), br(), br(),
+          br(), br(),
           fluidRow(
             column(
               width = 12,
               align = "center",
-              actionButton("next-btn-4", "Submit", class = "next-btn")
+              actionButton("submit-btn", "Submit", class = "next-btn")
             )
           )
         )
@@ -207,14 +199,14 @@ ui <- dashboardPage(
 )
 
 
-uiFunc <- function(req) {
-    if (!has_auth_code(parseQueryString(req$QUERY_STRING))) {
-        authorization_url <- oauth2.0_authorize_url(api, app, scope = scope)
-        return(tags$script(HTML(sprintf(
-            "location.replace(\"%s\");",
-            authorization_url
-        ))))
-    } else {
-        ui
-    }
-}
+# uiFunc <- function(req) {
+#     if (!has_auth_code(parseQueryString(req$QUERY_STRING))) {
+#         authorization_url <- oauth2.0_authorize_url(api, app, scope = scope)
+#         return(tags$script(HTML(sprintf(
+#             "location.replace(\"%s\");",
+#             authorization_url
+#         ))))
+#     } else {
+#         ui
+#     }
+# }
