77f195ccd23dc970d839c5dd8776f59d5d106ae8
rrchai <73901500+rrchai@users.noreply.github.com>
2023-07-25 00:46:48 -0400

    test deploy

--- server.R
+++ server.R
@@ -5,28 +5,28 @@
 # using the session token.  https://www.synapse.org
 
 shinyServer(function(input, output, session) {
-    # params <- parseQueryString(isolate(session$clientData$url_search))
-    # if (!has_auth_code(params)) {
-    #     return()
-    # }
-    # redirect_url <- paste0(
-    #     api$access, "?", "redirect_uri=", app_url, "&grant_type=",
-    #     "authorization_code", "&code=", params$code
-    # )
-    # # get the access_token and userinfo token
-    # req <- POST(redirect_url, encode = "form", body = "", authenticate(app$key, app$secret,
-    #     type = "basic"
-    # ), config = list())
-    # # Stop the code if anything other than 2XX status code is returned
-    # stop_for_status(req, task = "get an access token")
-    # token_response <- content(req, type = NULL)
-    # access_token <- token_response$access_token
-    # 
-    # session$userData$access_token <- access_token
-    # 
-    # # Login to synapse
-    # syn$login(authToken = access_token, rememberMe = FALSE)
-    
+    params <- parseQueryString(isolate(session$clientData$url_search))
+    if (!has_auth_code(params)) {
+        return()
+    }
+    redirect_url <- paste0(
+        api$access, "?", "redirect_uri=", app_url, "&grant_type=",
+        "authorization_code", "&code=", params$code
+    )
+    # get the access_token and userinfo token
+    req <- POST(redirect_url, encode = "form", body = "", authenticate(app$key, app$secret,
+        type = "basic"
+    ), config = list())
+    # Stop the code if anything other than 2XX status code is returned
+    stop_for_status(req, task = "get an access token")
+    token_response <- content(req, type = NULL)
+    access_token <- token_response$access_token
+
+    session$userData$access_token <- access_token
+
+    # Login to synapse
+    syn$login(authToken = access_token, rememberMe = FALSE)
+
     output$user <- renderUser({
       dashboardUser(
         name = "Awesome user",
--- ui.R
+++ ui.R
@@ -199,14 +199,14 @@ ui <- dashboardPage(
 )
 
 
-# uiFunc <- function(req) {
-#     if (!has_auth_code(parseQueryString(req$QUERY_STRING))) {
-#         authorization_url <- oauth2.0_authorize_url(api, app, scope = scope)
-#         return(tags$script(HTML(sprintf(
-#             "location.replace(\"%s\");",
-#             authorization_url
-#         ))))
-#     } else {
-#         ui
-#     }
-# }
+uiFunc <- function(req) {
+    if (!has_auth_code(parseQueryString(req$QUERY_STRING))) {
+        authorization_url <- oauth2.0_authorize_url(api, app, scope = scope)
+        return(tags$script(HTML(sprintf(
+            "location.replace(\"%s\");",
+            authorization_url
+        ))))
+    } else {
+        ui
+    }
+}
